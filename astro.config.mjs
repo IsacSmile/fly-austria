ac0e8befa57d4e795900a18e7b6e1cc07e7ee6aa
IsacSmile <94504780+IsacSmile@users.noreply.github.com>
2026-08-29 17:00:55 +0530

    Configure @astrojs/vercel adapter for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://flyaustria.in',
   output: 'static',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   integrations: [
     mdx(),
     sitemap(),
